bf7da52965b0e5e8aac62c13b73cecf10258f7bb
Fatih Kaya <fatihkaya125004@gmail.com>
2025-12-30 21:16:44 +0300

    initial commit

--- src/main/java/com/hospital/model/Yatak.java
+++ src/main/java/com/hospital/model/Yatak.java
@@ -18,8 +18,7 @@ public class Yatak {
     private String tcNo;
     private String hastaAdSoyad;
 
-    public Yatak() {
-    }
+
 
     public Yatak(int id, String blokNo, int katNo, String odaNumarasi, String yatakNumarasi,
                  String odaTuru, int yatakSayisi, String bakimTuru, YatakDurumu durum) {
